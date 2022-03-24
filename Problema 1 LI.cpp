7c46d5387b590e54df0168532c6595dcf8c7e622
Edgar Verjan Santoyo <93297400+EdgarVerjan@users.noreply.github.com>
2022-03-24 15:49:31 -0700

    Add files via upload

---
+++ Problema 1 LI.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+/**
+Problema 1
+Sara Mendez vende biblias a razon de $3.00 cada una, mas
+$0.65 por estampillas postales y manejo. Calcular sus
+ingresos de dos semanas en las que vendio 16 biblias.
+**/
+//Autor: EdgarVerjan
+//Fecha: 23/03/2022
+int main() {
+    double x, y, z, u, v;
+    x=3;
+    y=0.65;
+    z=16;
+    u=x+y;
+    v=u*z;
+    cout<<"Los ingresos en estas 2 semanas de la venta de biblias mas estampillas es de: ";
+    cout<<v;
+}
