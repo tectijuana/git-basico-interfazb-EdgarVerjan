1349ca5832fe7d7244250b50ad6e8f7d3ce16a1b
Edgar Verjan Santoyo <93297400+EdgarVerjan@users.noreply.github.com>
2022-03-24 20:33:34 -0700

    Add files via upload

---
+++ Problema 6 LI.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+/**
+Problema 6
+De cuantas maneras se pueden combinar cuartos, dos, quintos y
+centavos para dar 50 cts.?
+**/
+//Autor: EdgarVerjan
+//Fecha: 23/03/2022
+int main() {
+    double u,v,w,x,y,z;
+    x=25;
+    y=2;
+    z=10;
+    u=1;
+
+    cout<<"L cantidad de combinaciones es de: 33 formas"<<endl;
+}
